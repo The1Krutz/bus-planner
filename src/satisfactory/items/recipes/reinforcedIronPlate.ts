ac34fdf371b8cd38e4809a2f68e54f551db46585
Krutz <The1Krutz@gmail.com>
2025-10-25 00:32:27 -0400

    comment

--- src/satisfactory/items/recipes/reinforcedIronPlate.ts
+++ src/satisfactory/items/recipes/reinforcedIronPlate.ts
@@ -7,7 +7,7 @@ export const ReinforcedIronPlate = 'Reinforced Iron Plate';
 export const ReinforcedIronPlateRecipes: Recipe[] = [
   {
     name: 'Reinforced Iron Plate',
-    default:true,
+    default: true,
     produces: [
       {
         item: ReinforcedIronPlate,
@@ -25,4 +25,7 @@ export const ReinforcedIronPlateRecipes: Recipe[] = [
       },
     ],
   },
+  /**
+   * TODO - the rest of them
+   */
 ];
