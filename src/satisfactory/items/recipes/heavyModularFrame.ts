4b2b7d2a1f5802b48d37c3865bc3ff5af7de66db
Krutz <The1Krutz@gmail.com>
2025-10-27 13:32:03 -0400

    #8 hmf

--- src/satisfactory/items/recipes/heavyModularFrame.ts
+++ src/satisfactory/items/recipes/heavyModularFrame.ts
@@ -1,5 +1,9 @@
 import type { Recipe } from '..';
-import { Cable } from './cable';
+import { EncasedIndustrialBeam } from './encasedIndustrialBeam';
+import { Concrete, Rubber } from './importOnlyItems';
+import { ModularFrame } from './modularFrame';
+import { Screws } from './screws';
+import { SteelPipe } from './steelPipe';
 
 export const HeavyModularFrame = 'Heavy Modular Frame';
 
@@ -7,17 +11,32 @@ export const HeavyModularFrameRecipes: Recipe[] = [
   {
     name: 'Heavy Modular Frame',
     default: true,
-    produces: [
-      {
-        item: HeavyModularFrame,
-        rate: 0,
-      },
-    ],
+    produces: [{ item: HeavyModularFrame, rate: 2 }],
     consumes: [
-      {
-        item: Cable,
-        rate: 0,
-      },
+      { item: ModularFrame, rate: 10 },
+      { item: SteelPipe, rate: 40 },
+      { item: EncasedIndustrialBeam, rate: 10 },
+      { item: Screws, rate: 240 },
+    ],
+  },
+  {
+    name: 'Heavy Encased Frame',
+    produces: [{ item: HeavyModularFrame, rate: 2.8125 }],
+    consumes: [
+      { item: ModularFrame, rate: 7.5 },
+      { item: EncasedIndustrialBeam, rate: 9.375 },
+      { item: SteelPipe, rate: 33.75 },
+      { item: Concrete, rate: 20.625 },
+    ],
+  },
+  {
+    name: 'Heavy Flexible Frame',
+    produces: [{ item: HeavyModularFrame, rate: 3.75 }],
+    consumes: [
+      { item: ModularFrame, rate: 18.75 },
+      { item: EncasedIndustrialBeam, rate: 11.25 },
+      { item: Rubber, rate: 75 },
+      { item: Screws, rate: 390 },
     ],
   },
 ];
