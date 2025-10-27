2f48287b3af58feaf4074a965927f042c626c39c
Krutz <The1Krutz@gmail.com>
2025-10-27 13:35:49 -0400

    #8 smart plating

--- src/satisfactory/items/recipes/smartPlating.ts
+++ src/satisfactory/items/recipes/smartPlating.ts
@@ -1,5 +1,7 @@
 import type { Recipe } from '..';
-import { Cable } from './cable';
+import { Plastic } from './importOnlyItems';
+import { ReinforcedIronPlate } from './reinforcedIronPlate';
+import { Rotor } from './rotor';
 
 export const SmartPlating = 'Smart Plating';
 
@@ -7,17 +9,19 @@ export const SmartPlatingRecipes: Recipe[] = [
   {
     name: 'Smart Plating',
     default: true,
-    produces: [
-      {
-        item: SmartPlating,
-        rate: 0,
-      },
-    ],
+    produces: [{ item: SmartPlating, rate: 2 }],
     consumes: [
-      {
-        item: Cable,
-        rate: 0,
-      },
+      { item: ReinforcedIronPlate, rate: 2 },
+      { item: Rotor, rate: 2 },
+    ],
+  },
+  {
+    name: 'Plastic Smart Plating',
+    produces: [{ item: SmartPlating, rate: 5 }],
+    consumes: [
+      { item: ReinforcedIronPlate, rate: 2.5 },
+      { item: Rotor, rate: 2.5 },
+      { item: Plastic, rate: 7.5 },
     ],
   },
 ];
